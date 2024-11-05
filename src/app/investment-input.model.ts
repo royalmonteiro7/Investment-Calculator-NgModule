5d21540b47c83d59af4a32744d49b1a928c2394e
Royal Monteiro <royalmonteiro7@gmail.com>
2024-11-05 12:36:12 +0530

    5

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,13 @@
+export interface InvestmentInput {
+  initialInvestment: number;
+  duration: number;
+  expectedReturn: number;
+  annualInvestment: number;
+}
+
+// export type investmentInput = {
+//   initialInvestment: number;
+//   duration: number;
+//   expectedReturn: number;
+//   annualInvestment: number;
+// };
---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,23 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  private investmentService = inject(InvestmentService);
+
+  // get results() {
+  //   return this.investmentService.resultsData;
+  // }
+
+  // results = computed(() => {
+  //   return this.investmentService.resultsData();
+  // });
+  results = this.investmentService.resultsData.asReadonly();
+}
